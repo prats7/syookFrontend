5465174224986376df3e481e9267326b9fadf6eb
Pratyush Vishal <pratyushvishal04@gmail.com>
2021-01-21 21:11:27 +0530

    Heroku deploy

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
+const path = require('path')
 
 const app = express();
 
 //Bodyparser Middleware
 app.use(express.json());
-
+app.use(cors());
 //Connect to db
 const db = require('./config/mongoose');
 
 //Use routes
 app.use('/api/notes', require('./routes/api/notes'));
 
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
+
 //Express server
-const PORT = 9050 || process.env.PORT;
+const PORT = process.env.PORT || 9050;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
