1dd3e9baa0e8e2e888f449438e5c2f92d33eae7f
Pratyush Vishal <pratyushvishal04@gmail.com>
2021-01-21 13:21:23 +0530

    connect mongodb

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/syook');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'error in connectiing to DB'));
+
+
+db.once('open', function () {
+  console.log('Successfully connected to database :: MongoDB');
+});
+
+module.exports = db;
