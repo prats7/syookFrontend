b9356af40392dea312e39411ab6476b3991de816
Pratyush Vishal <pratyushvishal04@gmail.com>
2021-01-21 21:17:27 +0530

    Heroku deploy

--- server.js
+++ server.js
@@ -7,18 +7,21 @@ const app = express();
 
 //Bodyparser Middleware
 app.use(express.json());
-app.use(cors());
 //Connect to db
 const db = require('./config/mongoose');
 
 //Use routes
 app.use('/api/notes', require('./routes/api/notes'));
 
-app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
-})
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
 
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 //Express server
 const PORT = process.env.PORT || 9050;
 
